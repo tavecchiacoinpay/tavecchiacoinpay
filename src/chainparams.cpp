b8fcfc6799c2d9d6f31d7872b662b82e731f4b99
Hamza Tariq <hamzatrq116@gmail.com>
2020-10-08 16:07:40 +0500

    Updating pchMessageStart

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -113,10 +113,10 @@ public:
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 32-bit integer with any alignment.
          */
-        pchMessageStart[0] = 0xf9;
+        pchMessageStart[0] = 0xa9;
         pchMessageStart[1] = 0xbe;
-        pchMessageStart[2] = 0xb4;
-        pchMessageStart[3] = 0xd9;
+        pchMessageStart[2] = 0xb3;
+        pchMessageStart[3] = 0x01;
         nDefaultPort = 8333;
         nPruneAfterHeight = 100000;
 
