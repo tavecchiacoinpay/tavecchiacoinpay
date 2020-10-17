f60b2266ec5bc4ee76ef584ca0834f54bbc9666a
Hamza Tariq <hamzatrq116@gmail.com>
2020-10-17 18:25:39 +0500

    Premining all

--- src/validation.cpp
+++ src/validation.cpp
@@ -1152,6 +1152,13 @@ CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
     CAmount nSubsidy = 50 * COIN;
     // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
     nSubsidy >>= halvings;
+
+    if (nHeight === 2) {
+        CAmount preMine = 999999900 * COIN;
+    }
+    if (halvings >= 3)
+        return 0;
+
     return nSubsidy;
 }
 
