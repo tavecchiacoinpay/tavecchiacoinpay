cdcfaed1e2411848edee71d0020166acf38b0f9d
Hamza Tariq <hamzatrq116@gmail.com>
2020-10-20 16:50:55 +0500

    Changing seeds

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -130,8 +130,8 @@ public:
         // This is fine at runtime as we'll fall back to using them as a oneshot if they dont support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
-        vSeeds.emplace_back("seeda.tavecchiacoinpay.com");
-        vSeeds.emplace_back("seedb.tavecchiacoinpay.com");
+        vSeeds.emplace_back("142.93.5.112");
+        vSeeds.emplace_back("64.227.23.198");
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,127);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
