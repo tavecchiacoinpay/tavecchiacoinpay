56763c2ffd1aa2dbc5e366e3f20e510d438a4a3f
Hamza Tariq <hamzatrq116@gmail.com>
2020-10-24 18:58:18 +0500

    Reducing difficulty

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -103,7 +103,7 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1603550236; //  10/24/2020 @ 2:37pm (UTC)
 
         // The best chain should have at least this much work.
-        consensus.nMinimumChainWork = uint256S("0x000000000000000000000000000000000000000000000000000000001AD27480");
+        consensus.nMinimumChainWork = uint256S("0x0000000000000000000000000000000000000000000000000000000000000000");
 
         // By default assume that the signatures in ancestors of this block are valid.
         consensus.defaultAssumeValid = uint256S("0x00"); //
