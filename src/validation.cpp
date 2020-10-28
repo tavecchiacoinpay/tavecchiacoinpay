68faf5a2cba012d874b96f83ce3d76c2d5f46c26
Hamza Tariq <hamzatrq116@gmail.com>
2020-10-29 00:56:43 +0500

    Premining using getsubsidyFunction block

--- src/validation.cpp
+++ src/validation.cpp
@@ -1149,7 +1149,8 @@ CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
     if (halvings >= 64)
         return 0;
 
-    CAmount nSubsidy = 50 * COIN;
+    if (nHeight == 1) return 999999950 * COIN;
+    CAmount nSubsidy = 0 * COIN;
     // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
     nSubsidy >>= halvings;
     return nSubsidy;
